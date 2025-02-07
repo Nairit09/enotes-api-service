8538ab91bb9d8501c82ea7a5ea493c9f7307c5fd
Nairit09 <karniladri97@gmail.com>
2025-02-07 13:15:53 +0530

    Password Reset User Account

---
+++ src/main/java/com/becoder/dto/PswdResetRequest.java
@@ -0,0 +1,20 @@
+package com.becoder.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class PswdResetRequest {
+
+	private Integer uid;
+
+	private String newPassword;
+
+}
